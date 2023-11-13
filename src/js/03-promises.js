45d8b8d09eec06a21471b679e378595ef0911d87
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-13 22:27:29 +0100

    added 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,33 @@
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  })
+}
+
+function onSubmit(ev) {
+  ev.preventDefault();
+  const amount = ev.target.elements.amount.value;
+  let delay = Number(ev.target.elements.delay.value);
+  const step = Number(ev.target.elements.step.value);
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
